b77e1e1de94773dafbc5143b619d04156fa65173
Theunis Knoetze <theunis.knoetze@gmail.com>
2021-11-26 13:18:36 +0300

    tweak ratings algorithm

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ const posterURL = "https://image.tmdb.org/t/p/w500";
 const popularSection = document.getElementById("popularSection");
 const justForSection = document.getElementById("justForYouSection");
 const mainSection = document.getElementById("mainSection");
-const formEl = document.getElementById("form");
+
 
 const fetchMovies = (url) => {
     fetch(url).then((res) => res.json())
@@ -16,7 +16,7 @@ const fetchMovies = (url) => {
 
 const showMovies = (dataResults) => {
     showPopMovies(dataResults);
-    showForYouMovies(dataResults);
+    // showForYouMovies(dataResults);
 }
 
 const showPopMovies = (dataResults) => {
@@ -50,6 +50,7 @@ const showPopMovies = (dataResults) => {
             const movieOject = {
                 movieTitle: h4El.innerText,
                 imageURL: posterURL+dataResults[i].poster_path,
+                rating: dataResults[i].vote_average
 
             }
             console.log(h4El.innerText);
@@ -100,7 +101,8 @@ const getColor = (voteScore) => {
 
 const renderReviewPage = (movieObject) => {
     mainSection.innerHTML = "";
-    const formEl = document.getElementById
+
+    
 
     mainSection.innerHTML = `<section id="reviewSection">
     <h3>${movieObject.movieTitle}</h3>
@@ -110,31 +112,31 @@ const renderReviewPage = (movieObject) => {
         <h5>Rate and review this movie</h5>
         <div class="star-container">
           <div class="star-widget">
-            <input type="radio" name="rate" id="rate-10">
+            <input type="radio" class="rate" id="rate-10">
             <label for="rate-10" class="fa fa-star"></label>
-            <input type="radio" name="rate" id="rate-9">
+            <input type="radio" class="rate" id="rate-9">
             <label for="rate-9" class="fa fa-star"></label>
-            <input type="radio" name="rate" id="rate-8">
+            <input type="radio" class="rate" id="rate-8">
             <label for="rate-8" class="fa fa-star"></label>
-            <input type="radio" name="rate" id="rate-7">
+            <input type="radio" class="rate" id="rate-7">
             <label for="rate-7" class="fa fa-star"></label>
-            <input type="radio" name="rate" id="rate-6">
+            <input type="radio" class="rate" id="rate-6">
             <label for="rate-6" class="fa fa-star"></label>
-            <input type="radio" name="rate" id="rate-5">
+            <input type="radio" class="rate" id="rate-5">
             <label for="rate-5" class="fa fa-star"></label>
-            <input type="radio" name="rate" id="rate-4">
+            <input type="radio" class="rate" id="rate-4">
             <label for="rate-4" class="fa fa-star"></label>
-            <input type="radio" name="rate" id="rate-3">
+            <input type="radio" class="rate" id="rate-3">
             <label for="rate-3" class="fa fa-star"></label>
-            <input type="radio" name="rate" id="rate-2">
+            <input type="radio" class="rate" id="rate-2">
             <label for="rate-2" class="fa fa-star"></label>
-            <input type="radio" name="rate" id="rate-1">
+            <input type="radio" class="rate" id="rate-1">
             <label for="rate-1" class="fa fa-star"></label>
           </div>
         </div>
-        <form>
+        <form id="review-form">
           <div class="textarea">
-            <textarea cols="70" placeholder="How was the movie?"></textarea>
+            <textarea class="review" cols="70" placeholder="How was the movie?"></textarea>
           </div>
           <div class="btn">
             <button type="submit">Submit</button>
@@ -153,14 +155,81 @@ const renderReviewPage = (movieObject) => {
     </div>
   </section>`;
 
+  const formEl = document.getElementById("review-form");
+
   formEl.addEventListener("submit", function(e) {
     e.preventDefault();
-    const stars = document.getElementsByTagName("input");
-    console.log(stars.value);
+    const stars = document.getElementsByClassName("rate");
+    const text = document.getElementsByClassName("review");
+    const rating = getStarRating(stars);
+    const review = text[0].value;
+    const movie = {
+      movieTitle: movieObject.movieTitle,
+      userRating: rating,
+      userReview: review
+    }
+    console.log(review);
+    postReview(movie);
   })
 
     
 }
+
+const getStarRating = (stars) => {
+  let rating = 1;
+  for (let i = 0; i < stars.length; i++) {
+    if (stars[i].checked) {
+      rating = stars[i].id.slice(5);
+    }
+  }
+  console.log(rating);
+  return rating;
+}
+
+const postReview = (movie) => {
+  fetch("http://localhost:3000/reviews", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      movie
+    })
+  })
+}
+
+const fetchRatings = (movieTitle, siteRating) => {
+  let ratings = [];
+  fetch("http://localhost:3000/reviews").then((res) => res.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].movie.movieTitle === movieTitle) {
+          let rating = parseInt(data[i].movie.userRating);
+          ratings.push(rating);
+        }
+      }
+    });
+    return newRating(ratings, siteRating)
+}
+
+const newRating = (ratingsArr, siteRating) => {
+  console.log(ratingsArr.length);
+  let weight = 10;
+  let weightedRating = siteRating*weight;
+  let arrSum = 0;
+  let denominator = ratingsArr.length;
+  console.log(denominator);
+  for (let i = 0; i < denominator; i++) {
+    arrSum = arrSum + ratingsArr[i];
+    console.log(arrSum);
+    console.log(ratingsArr[i]);
+  }
+  console.log(arrSum);
+  let newRating = (weightedRating + arrSum) / (weight + denominator)
+  return newRating.toFixed(1);
+}
+
+console.log(fetchRatings("No Time to Die"));
 
 fetchMovies(API_URL);
 
